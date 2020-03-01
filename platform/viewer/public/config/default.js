8711eb352dfaa9b3cb07f62811398422c4f501dd
Youngjoon Choi <ejoonie@gmail.com>
2020-03-01 17:36:13 +0900

    aws dcm4chee

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -9,9 +9,9 @@ window.config = {
     dicomWeb: [
       {
         name: 'DCM4CHEE',
-        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoUriRoot: 'http://15.165.171.194:8080/dcm4chee-arc/aets/DCM4CHEE/wado',
+        qidoRoot: 'http://15.165.171.194:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoRoot: 'http://15.165.171.194:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
         qidoSupportsIncludeField: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
